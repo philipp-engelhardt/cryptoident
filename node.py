4a259804064fb5d03037ffa345403d91e25b9add
BGM99 <45606744+BGM99@users.noreply.github.com>
2024-07-19 16:43:06 +0200

    add new node service

--- node.py
+++ node.py
@@ -3,6 +3,28 @@ import threading
 import json
 import pickle
 import rpyc
+from rpyc import ThreadedServer
+
+
+class NodeService(rpyc.Service):
+    known_peers = set()
+
+    def on_connect(self, conn):
+        print("Connected to", conn)
+
+    def on_disconnect(self, conn):
+        print("Disconnected from", conn)
+
+    def exposed_get_data(self):
+        return "Hello from peer"
+
+    def exposed_register_peer(self, host, port):
+        self.known_peers.add((host, port))
+        return True
+
+    def exposed_get_known_peers(self):
+        return list(self.known_peers)
+
 
 
 class Node:
@@ -11,45 +33,19 @@ class Node:
         self.port = port
         self.peers = []
 
-    def start_server(self):
-        server = threading.Thread(target=self.server_thread)
+    def start_server(port):
+        server = ThreadedServer(NodeService, port=port)
         server.start()
 
-    def server_thread(self):
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.bind((self.host, self.port))
-        server_socket.listen(5)
-        print(f'Server started on {self.host}:{self.port}')
+    def connect_to_peer(self, host, port):
+        conn = rpyc.connect(host, port)
+        self.peers.append(conn)
+        print(f'Connected to peer {host}:{port}')
 
-        while True:
-            conn, addr = server_socket.accept()
-            print(f'Connected by {addr}')
-            client_thread = threading.Thread(target=self.client_handler, args=(conn,))
-            client_thread.start()
-
-    def client_handler(self, conn):
-        while True:
-            try:
-                data = conn.recv(4096)
-                if not data:
-                    break
-                self.handle_message(data)
-            except:
-                break
+    def register_with_peer(host, port, my_host, my_port):
+        conn = rpyc.connect(host, port)
+        conn.root.register_peer(my_host, my_port)
         conn.close()
-
-    def connect_to_peer(self, peer_host, peer_port):
-        peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        peer_socket.connect((peer_host, peer_port))
-        self.peers.append(peer_socket)
-        print(f'Connected to peer {peer_host}:{peer_port}')
-
-    def broadcast_transaction(self, transaction):
-        for peer in self.peers:
-            try:
-                peer.sendall(json.dumps(transaction).encode())
-            except:
-                self.peers.remove(peer)
 
     def broadcast_blockchain(self, blockchain):
         for peer in self.peers:
@@ -57,7 +53,3 @@ class Node:
                 peer.sendall(pickle.dumps(blockchain))
             except:
                 self.peers.remove(peer)
-
-    def handle_message(self, message):
-        chain = pickle.loads(message)
-        print(f'Received blockchain: {chain}')
