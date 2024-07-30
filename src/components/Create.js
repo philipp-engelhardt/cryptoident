b03e564d36371735350a3f358a8ca557246306d6
philipp-engelhardt <123982724+philipp-engelhardt@users.noreply.github.com>
2024-07-30 12:17:28 +0200

    optical changes to create page and sidebar

--- src/components/Create.js
+++ src/components/Create.js
@@ -14,12 +14,12 @@ const Create = () => {
 
   const handleFileChange = (e, setFile) => {
     const file = e.target.files[0];
-    const validFormats = ['image/png', 'image/jpeg', 'application/pdf']; // Beispiel für gültige Formate
+    const validFormat = '.key';
 
-    if (file && validFormats.includes(file.type)) {
+    if (file && file.name.endsWith(validFormat)) {
       setFile(file);
     } else {
-      alert('Ungültiges Dateiformat. Bitte wählen Sie eine PNG, JPEG oder PDF Datei.');
+      alert('Ungültiges Dateiformat. Bitte wählen Sie eine .key Datei.');
       e.target.value = ''; // Reset the input if the file format is invalid
     }
   };
@@ -37,32 +37,38 @@ const Create = () => {
         <div className="create-main">
           <h2>Create New Entry</h2>
           <div className="create-box-container">
-            <div className="create-box">
+            <div className="create-box-single">
               <input
                 type="file"
                 id="file1"
+                accept=".key"
                 onChange={(e) => handleFileChange(e, setFile1)}
               />
-              <label htmlFor="file1" className={file1 ? 'active' : ''}>Select File</label>
-              {file1 && <p>{file1.name}</p>}
+              <label htmlFor="file1" className={file1 ? 'active' : ''}>
+                {file1 ? file1.name : 'Upload Person Public Key'}
+              </label>
             </div>
             <div className="create-box">
               <input
                 type="file"
                 id="file2"
+                accept=".key"
                 onChange={(e) => handleFileChange(e, setFile2)}
               />
-              <label htmlFor="file2" className={file2 ? 'active' : ''}>Select File</label>
-              {file2 && <p>{file2.name}</p>}
+              <label htmlFor="file2" className={file2 ? 'active' : ''}>
+                {file2 ? file2.name : 'Upload Validator Public Key'}
+              </label>
             </div>
             <div className="create-box">
               <input
                 type="file"
                 id="file3"
+                accept=".key"
                 onChange={(e) => handleFileChange(e, setFile3)}
               />
-              <label htmlFor="file3" className={file3 ? 'active' : ''}>Select File</label>
-              {file3 && <p>{file3.name}</p>}
+              <label htmlFor="file3" className={file3 ? 'active' : ''}>
+                {file3 ? file3.name : 'Upload Validator Private Key'}
+              </label>
             </div>
           </div>
           <form className="create-input-container" onSubmit={handleSubmit}>
