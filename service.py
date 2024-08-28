9acc766d895dbc8f196df307ca2ba270edd0a88a
BGM99 <45606744+BGM99@users.noreply.github.com>
2024-08-28 14:09:42 +0200

    add service.py

---
+++ service.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/generate_wallet', methods=['GET'])
+def generate_wallet():
+    return jsonify(), 200
+
+
+@app.route('/block/<int:block_height>', methods=['GET'])
+def get_block_details(block_height):
+    return jsonify(block_height), 200
+
+@app.route('/latest_blocks', methods=['GET'])
+def get_latest_blocks(block_height):
+    return jsonify(block_height), 200
+
+
+@app.route('/create_new_block', methods=['POST'])
+def create_new_block():
+    new_item = request.json
+    return jsonify(new_item), 200
+
+
+@app.route('/search', methods=['GET'])
+def search_person():
+    query = request.args.get('q', '').lower()
+    return jsonify(query), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
